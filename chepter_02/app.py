1aae223574ac0ae3cda938ab2fc876046d662a6d
rahul117590 <rahulkumar117590@gmail.com>
2026-07-09 02:25:27 -0700

    userloginpage

--- chepter_02/app.py
+++ chepter_02/app.py
@@ -1,13 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app=Flask(__name__)
 # this is used to connect the app with the browser
 
 
 @app.route("/")
-def home():
-    return render_template('home.html')
+def login():
+    return render_template('login.html')
 
+@app.route("/submit",methods=['POST'])
+def submit():
+    username=request.form.get('username')#read the user name
+    password=request.form.get('password')# read the password
+
+    if username=='rahul' and password=='123':
+        return render_template('welcome.html', name=username)
+    
+    else:
+        return 'invalid credentials'
 
 
 # for run the file 
